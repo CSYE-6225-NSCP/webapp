2c16d474be05c6b9d9dd9b4c9f35333f415c93c6
malepatic <malepati.c@northeastern.edu>
2025-01-29 16:02:00 -0500

    Written readme with adding techstack and commands and added functionality that dont require for all payloads

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', healthCheckRouter);
 
-const PORT = process.env.PORT || process.env.HOST;
+const PORT = process.env.PORT || process.env.SERVER_PORT;
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
